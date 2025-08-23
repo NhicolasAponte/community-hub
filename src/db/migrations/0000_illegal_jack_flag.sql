7a3000a6f73590de04b3a39c361cd5685007986b
NhicolasAponte <Nhic.Aponte@gmail.com>
2025-08-23 09:55:19 -0500

    updated newsletter 'content' column type to 'text' to allow long content

--- src/db/migrations/0000_illegal_jack_flag.sql
+++ src/db/migrations/0000_illegal_jack_flag.sql
@@ -8,8 +8,8 @@ CREATE TABLE "event" (
 --> statement-breakpoint
 CREATE TABLE "newsletter" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"title" varchar NOT NULL,
-	"content" varchar(255) NOT NULL,
+	"title" varchar(255) NOT NULL,
+	"content" text NOT NULL,
 	"date" varchar(255) NOT NULL
 );
 --> statement-breakpoint
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,42 +1,33 @@
-import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { pgTable, text, uuid, varchar } from "drizzle-orm/pg-core";
 
+export const vendorTable = pgTable("vendor", {
+  id: uuid("id").primaryKey().defaultRandom().notNull(),
+  name: varchar("name").notNull(),
+  description: varchar("description", { length: 255 }).notNull(),
+  email: varchar("email", { length: 255 }),
+  phone: varchar("phone", { length: 255 }),
+  address: varchar("address", { length: 255 }),
+  services: varchar("services", { length: 255 }),
+  links: varchar("links", { length: 255 }),
+  // instagram: varchar("instagram", { length: 255 }),
+  // twitter: varchar("twitter", { length: 255 }),
+  // linkedin: varchar("linkedin", { length: 255 }),
+  // facebook: varchar("facebook", { length: 255 }),
+});
 
-export const vendorTable = pgTable(
-    "vendor", 
-    {
-    id: uuid("id").primaryKey().defaultRandom().notNull(),
-    name: varchar("name").notNull(),
-    description: varchar("description", { length: 255 }).notNull(),
-    email: varchar("email", { length: 255 }),
-    phone: varchar("phone", { length: 255 }),
-    address: varchar("address", { length: 255 }),
-    services: varchar("services", { length: 255 }),
-    links: varchar("links", { length: 255 }),
-    // instagram: varchar("instagram", { length: 255 }),
-    // twitter: varchar("twitter", { length: 255 }),
-    // linkedin: varchar("linkedin", { length: 255 }),
-    // facebook: varchar("facebook", { length: 255 }),
-})
+export const eventTable = pgTable("event", {
+  id: uuid("id").primaryKey().defaultRandom().notNull(),
+  name: varchar("name").notNull(),
+  description: varchar("description", { length: 255 }).notNull(),
+  date: varchar("date", { length: 255 }).notNull(),
+  location: varchar("location", { length: 255 }).notNull(),
+  // badges: varchar("tags", { length: 255 }),
+  // badges let you add a badge like "pride", "holiday", "alcohol-free", etc.
+});
 
-export const eventTable = pgTable(
-    "event", 
-    {
-    id: uuid("id").primaryKey().defaultRandom().notNull(),
-    name: varchar("name").notNull(),
-    description: varchar("description", { length: 255 }).notNull(),
-    date: varchar("date", { length: 255 }).notNull(),
-    location: varchar("location", { length: 255 }).notNull(),
-    // badges: varchar("tags", { length: 255 }),
-        // badges let you add a badge like "pride", "holiday", "alcohol-free", etc.
-    }
-)
-
-export const newsletterTable = pgTable(
-    "newsletter",
-    {
-        id: uuid("id").primaryKey().defaultRandom().notNull(),
-        title: varchar("title", { length: 255 }).notNull(),
-        content: varchar("content").notNull(),
-        date: varchar("date", { length: 255 }).notNull(),
-    }
-)
+export const newsletterTable = pgTable("newsletter", {
+  id: uuid("id").primaryKey().defaultRandom().notNull(),
+  title: varchar("title", { length: 255 }).notNull(),
+  content: text("content").notNull(),
+  date: varchar("date", { length: 255 }).notNull(),
+});
